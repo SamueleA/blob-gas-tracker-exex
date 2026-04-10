d6e901f5f0cd74f1f57fdaed27e79e38345d33ff
samuelea <batonenbois@gmail.com>
2026-04-10 12:30:48 -0400

    added rocksdb database

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,36 @@ use std::{
 };
 
 use alloy_consensus::Transaction;
-use alloy_primitives::{Address, BlockNumber};
+use alloy_primitives::Address;
 use futures_util::{FutureExt, TryStreamExt};
-use reth::{
-    api::{BlockBody, FullNodeComponents},
-    builder::NodeTypes,
-    primitives::EthPrimitives,
-};
+use reth::{api::FullNodeComponents, builder::NodeTypes, primitives::EthPrimitives};
 use reth_exex::{ExExContext, ExExEvent};
 use reth_node_ethereum::EthereumNode;
-use reth_tracing::tracing::info;
+use rocksdb::{DB, Options};
+use serde::{Deserialize, Serialize};
 
+const GAS_DB_PATH: &str = "./gas_db";
+
+#[derive(Serialize, Deserialize)]
+struct GasData {
+    gas_consumed: u64,
+}
 struct MyExEx<Node: FullNodeComponents> {
     ctx: ExExContext<Node>,
-    /// Block gas transaction by To address
+    /// Block gas transaction by To address (TODO: remove this)
     gas_consumed_by_address: HashMap<Address, u64>,
+    /// DB for gas consumption by address
+    gas_db: DB,
 }
 
 impl<Node: FullNodeComponents> MyExEx<Node> {
     fn new(ctx: ExExContext<Node>) -> Self {
+        let gas_db = db_init(GAS_DB_PATH);
+
         Self {
             ctx,
             gas_consumed_by_address: HashMap::new(),
+            gas_db,
         }
     }
 }
@@ -53,6 +61,20 @@ impl<Node: FullNodeComponents<Types: NodeTypes<Primitives = EthPrimitives>>> Fut
                         let blob_gas_consumed = eip4844.blob_gas_used().unwrap_or_default();
                         let destination = eip4844.to().unwrap_or_default();
 
+                        let gas_db_entry = this.gas_db.get(destination).unwrap();
+
+                        let mut current_gas: GasData = match gas_db_entry {
+                            Some(value_bytes) => serde_json::from_slice(&value_bytes).unwrap(),
+                            None => GasData { gas_consumed: 0 },
+                        };
+
+                        current_gas.gas_consumed =
+                            current_gas.gas_consumed.wrapping_sub(blob_gas_consumed);
+
+                        let updated_gas_db_entry = serde_json::to_vec(&current_gas).unwrap();
+
+                        this.gas_db.put(destination, updated_gas_db_entry).unwrap();
+
                         *this
                             .gas_consumed_by_address
                             .entry(destination)
@@ -77,6 +99,20 @@ impl<Node: FullNodeComponents<Types: NodeTypes<Primitives = EthPrimitives>>> Fut
                         let blob_gas_consumed = eip4844.blob_gas_used().unwrap_or_default();
                         let destination = eip4844.to().unwrap_or_default();
 
+                        let gas_db_entry = this.gas_db.get(destination).unwrap();
+
+                        let mut current_gas: GasData = match gas_db_entry {
+                            Some(value_bytes) => serde_json::from_slice(&value_bytes).unwrap(),
+                            None => GasData { gas_consumed: 0 },
+                        };
+
+                        current_gas.gas_consumed =
+                            current_gas.gas_consumed.wrapping_add(blob_gas_consumed);
+
+                        let updated_gas_db_entry = serde_json::to_vec(&current_gas).unwrap();
+
+                        this.gas_db.put(destination, updated_gas_db_entry).unwrap();
+
                         *this
                             .gas_consumed_by_address
                             .entry(destination)
@@ -92,6 +128,16 @@ impl<Node: FullNodeComponents<Types: NodeTypes<Primitives = EthPrimitives>>> Fut
 
         Poll::Ready(Ok(()))
     }
+}
+
+fn db_init(path: &str) -> DB {
+    let mut opts = Options::default();
+    opts.create_if_missing(true);
+    let db = match DB::open(&opts, path) {
+        Ok(db) => db,
+        Err(e) => panic!("failed to open database: {}", e),
+    };
+    db
 }
 
 fn main() -> eyre::Result<()> {
